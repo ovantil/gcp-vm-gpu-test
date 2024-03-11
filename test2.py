5effabf3633d2422f026d88cfab24d7adb502606
= <=>
2024-03-11 15:34:36 +0000

    ...

--- test2.py
+++ test2.py
@@ -1,16 +1,10 @@
 import os
 import cv2
-import json
 import time
 import torch
 import open_clip
-import numpy as np
-
-from torch.nn import DataParallel
-
 from PIL import Image
 from rich import print as rprint
-from sentence_transformers import util
 
 rprint("[bold]Loading model...[/bold]")
 load_start_time = time.time()
@@ -20,56 +14,36 @@ model, _, preprocess = open_clip.create_model_and_transforms(model_name, pretrai
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
-    model = DataParallel(model)
+    model = torch.nn.DataParallel(model, device_ids=[0, 1, 2, 3])  # Use 4 GPUs
     model.to(device)
 else:
     device = torch.device("mps")
     model.to(device)
 
-
 rprint(f"-> Model loaded in {time.time() - load_start_time:.2f} seconds")
 
 base_image_folder = "./data/base_images"
-base_image_paths = [
-    os.path.join(base_image_folder, img_name)
-    for img_name in os.listdir(base_image_folder)
-]
-
+base_image_paths = [os.path.join(base_image_folder, img_name) for img_name in os.listdir(base_image_folder)]
 BATCH_SIZE = 2000
-
 
 def load_and_preprocess_images(image_paths):
     for i in range(0, len(image_paths), BATCH_SIZE):
-        batch_paths = image_paths[i : i + BATCH_SIZE]
-        imgs = [
-            cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
-            for img_path in batch_paths
-        ]
+        batch_paths = image_paths[i: i + BATCH_SIZE]
+        imgs = [cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB) for img_path in batch_paths]
         imgs = [Image.fromarray(img).convert("RGB") for img in imgs]
         imgs = torch.stack([preprocess(img) for img in imgs]).to(device)
         yield imgs
 
-
 def encode_images(image_paths):
     rprint(f"-> Encoding {len(image_paths)} images...")
     start_time = time.time()
-    if not isinstance(image_paths, list):
-        image_paths = [
-            os.path.join(base_image_folder, img_name)
-            for img_name in os.listdir(base_image_folder)
-        ]
-
     encoded_images = []
     for batch_imgs in load_and_preprocess_images(image_paths):
         with torch.no_grad():
-            batch_encoded_imgs = model.encode_image(batch_imgs)
+            batch_encoded_imgs = model.module.encode_image(batch_imgs)
             encoded_images.append(batch_encoded_imgs)
-
-        rprint(
-            f"-> Encoded {len(batch_imgs)} images in {time.time() - start_time:.2f} seconds"
-        )
+        rprint(f"-> Encoded {len(batch_imgs)} images in {time.time() - start_time:.2f} seconds")
     return torch.cat(encoded_images)
-
 
 rprint("[bold]Encoding item images...[/bold]")
 encode_start_time = time.time()
@@ -78,15 +52,7 @@ item_image_folders = os.listdir(item_image_folders_path)
 image_paths = []
 for folder in item_image_folders:
     folder_path = os.path.join(item_image_folders_path, folder)
-
-    # ensure folder is a directory
-    if not os.path.isdir(folder_path):
-        continue
-
-    image_paths.extend(
-        [os.path.join(folder_path, img) for img in os.listdir(folder_path)]
-    )
+    if os.path.isdir(folder_path):
+        image_paths.extend([os.path.join(folder_path, img) for img in os.listdir(folder_path)])
 encoded_item_images = encode_images(image_paths)
-rprint(
-    f"-> Encoded {len(image_paths)} images in {time.time() - encode_start_time:.2f} seconds"
-)
+rprint(f"-> Encoded {len(image_paths)} images in {time.time() - encode_start_time:.2f} seconds")
